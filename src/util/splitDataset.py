a60bd93071af9268c18ab9cb0860127988774d88
mypoint27 <jesper7.choi@gmail.com>
2021-08-23 15:56:23 +0900

    data ratio changes

--- src/util/splitDataset.py
+++ src/util/splitDataset.py
@@ -55,6 +55,55 @@ def train_val_test_split(X, y, splitCount, unlabelSize, test_val_size, random_se
     return train_X, train_y, unlabel_X, unlabel_y, val_X, val_y, test_X, test_y
 
 
+def train_val_test_split2(X, y, test_val_size, unlabelSize, random_seed):    
+    
+    split = StratifiedShuffleSplit(n_splits=1, test_size=test_val_size, random_state = random_seed)
+    
+    test_val=[]
+    data = []    
+    train =[]
+    unlabel =[]
+    label=[]    
+    test =[]
+    val =[]
+    
+    for data_id, test_val_id in split.split(X,y):
+        
+        data.append(data_id)        
+        test_val.append(test_val_id)
+        
+        data_X = X.iloc[data[0]]
+        data_y = y.iloc[data[0]]
+                
+        test_val_X = X.iloc[test_val[0]]
+        test_val_y = y.iloc[test_val[0]]
+        
+        split2 = StratifiedShuffleSplit(n_splits=1, test_size=0.5, random_state =random_seed)    
+        for test_id, val_id in split2.split(test_val_X, test_val_y):
+            test.append(test_id)
+            val.append(val_id)
+            
+            test_X = test_val_X.iloc[test[0]]
+            test_y = test_val_y.iloc[test[0]]
+            
+            val_X = test_val_X.iloc[val[0]]
+            val_y = test_val_y.iloc[val[0]]            
+            
+            
+        split3 = StratifiedShuffleSplit(n_splits=1, test_size=unlabelSize, random_state=random_seed)        
+        for train_id, unlabel_id in split3.split(data_X, data_y):
+            
+            train.append(train_id)
+            unlabel.append(unlabel_id)
+            
+            train_X = data_X.iloc[train[0]]            
+            train_y = data_y.iloc[train[0]]
+            
+            unlabel_X = data_X.iloc[unlabel[0]]
+            unlabel_y = data_y.iloc[unlabel[0]]
+        
+        return  train_X, train_y, unlabel_X, unlabel_y, val_X, val_y, test_X, test_y
+
 def getTestValIndex(y, random_seed):
     # Anomal label
     anomalLabel = 1
@@ -72,10 +121,10 @@ def getTestValIndex(y, random_seed):
 
 def getDatasets(k, X, y):
     try:
-        assert (k < 10), f"k must be 1~10"
+        assert (k < 20), f"k must be 0~19"
     except AssertionError as e:
         raise
-    data = train_val_test_split(X, y, splitCount=10, unlabelSize=0.7, test_val_size=0.3, random_seed=k+72)
+    data = train_val_test_split2(X, y, test_val_size=0.3,  unlabelSize=0.5, random_seed=k+72)
 
     trainX = data[0].reset_index(drop=True)
     trainy = data[1].reset_index(drop=True)
